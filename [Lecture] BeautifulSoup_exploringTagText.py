4bda66b507947e13d4e617b34d90848674d90495
KyeongJun-Min <76139297+KyeongJun-Min@users.noreply.github.com>
2021-11-07 19:20:37 +0900

    Create [Lecture] BeautifulSoup_exploringTagText.py

---
+++ [Lecture] BeautifulSoup_exploringTagText.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get("https://workey.codeit.kr/ratings/index")
+rating_page = response.text
+
+soup = BeautifulSoup(rating_page, 'html.parser')
+
+program_title_tags = soup.select('td.program')
+
+program_titles = []
+for tag in program_title_tags:
+    print(tag.get_text())
+    program_titles.append(tag.get_text())
+
+print(program_titles)
+
+print(soup.select_one('td.program'))
