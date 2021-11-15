ed0ffcd96c474ee0df0c3d13febad20602ea66c7
KyeongJun-Min <76139297+KyeongJun-Min@users.noreply.github.com>
2021-11-16 03:25:30 +0900

    Update and rename 22. [Solving] Selenium_코스타그램_스크롤 이동하며 모든 데이터 접근_텍스트 슬라이싱 추가.py to 22. [Solving] Selenium_코스타그램_스크롤 이동하며 모든 데이터 접근_엑셀에 데이터 전부 저장.py

--- 22. [Solving] Selenium_코스타그램_스크롤 이동하며 모든 데이터 접근_엑셀에 데이터 전부 저장.py
+++ 22. [Solving] Selenium_코스타그램_스크롤 이동하며 모든 데이터 접근_엑셀에 데이터 전부 저장.py
@@ -1,3 +1,23 @@
+'''
+저번 영상에서 설명드렸던 코스타그램 스크래핑을 시작해 봅시다.
+이번 과제에서는 아래 동작들에 대한 Selenium 코드를 짜 주세요:
+웹 드라이버 생성 후 코스타그램 (https://workey.codeit.kr/costagram/index) 접속
+코스타그램 로그인
+웹 페이지 끝까지 스크롤
+각 썸네일(포스팅)을 클릭하고, 창이 뜨면 닫기 버튼 누르기
+웹 드라이버 종료
+웹사이트 로딩을 생각해서, wait도 추가해 주는 것 잊지 마세요!
+'''
+
+'''
+썸네일을 클릭하면 상세 정보가 뜨는데요, 이번 과제에서는 각 포스트의 이미지 주소를 추출한 다음, 출력해 주는 코드를 추가해 주세요.
+
+이미지 주소는 하이라이트된 div 태그의 style 속성 값에 있는 /images/costagram/posts/post0.jpg 부분입니다.
+
+이전 과제의 해답 코드는 템플릿 코드로 작성해 놨습니다.
+'''
+
+
 import time
 
 from selenium import webdriver
@@ -9,6 +29,7 @@ driver.implicitly_wait(3)
 
 wb = Workbook(write_only=True)
 ws = wb.create_sheet("코스타그램 data")
+ws.append(['이미지 주소', '내용', '해시태그', '좋아요 수', '댓글 수'])
 
 driver.get(url)
 
@@ -37,8 +58,16 @@ for thumb_nail in thumb_nails:
     split_image_url = driver.find_element_by_xpath("//div[@class='post-container__image']").get_attribute('style').split(" ")[1]
     image_url = split_image_url[5:-3]
 
-    print(image_url)
+    text_content = driver.find_element_by_xpath("//span[@class='content__text']").text
+    hash_tag = driver.find_element_by_css_selector("div.content__tag-cover").text
+    like_count = driver.find_element_by_xpath("//span[@class='content__like-count']").text
+    comment_count = driver.find_element_by_css_selector("span.content__comment-count").text
+
+    ws.append([image_url, text_content, hash_tag, like_count, comment_count])
+
     driver.find_element_by_css_selector("button.close-btn").click()
+
+wb.save("코스타그램.xlsx")
 
 driver.quit()
 
